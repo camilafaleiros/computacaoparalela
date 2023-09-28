57bd160cc0401572108af4d659df1b7dc7e3b4b0
Cloud User <ec2-user@ip-172-31-20-17.ec2.internal>
2023-09-28 21:39:02 +0000

    Multiplicação Versão Paralela

--- lab04/matrix_vector_Paralela.c
+++ lab04/matrix_vector_Paralela.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int l = 10, c = 10;
-int v[10];
+int v[10] = {1, 2, 3, 4, 5, 2, 6, 3, 4, 3};;
 int m[10][10] = {{1,1,2,2,3,3,4,4,5,5},
 			  {6,6,7,7,8,8,9,9,10,10},
 			  {1,1,2,2,3,3,4,4,5,5},
@@ -22,18 +22,13 @@ void *createThread(void *rank) {
   long mRank = (long)rank;
   int i, j;
   int loc = l / count_t;
-  int m = mRank * loc;
-  int n = (mRank + 1) * loc - 1;
-  for (i = m; i <= n; i++) {
+  int my_first_row = mRank * loc;
+  int my_last_row = (mRank + 1) * loc - 1;
+  for (i = my_first_row; i <= my_last_row; i++) {
     r[i] = 0;
     for (j = 0; j < c; j++) {
-      r[i] += (m[i][j]) * (v[j]);
+      r[i] += m[i][j] * v[j];
     }
-  }
-
-  for (int i = 0; i < 10; i++){
-    printf("R[%i]: ", i);
-    printf("%d\n", r[i]);
   }
   return NULL;
 }
@@ -43,13 +38,18 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             printf("M[%i][%i]: ", i, j);
-            printf("%d", m[i][j]);
+            printf("%d  ", m[i][j]);
         }
         printf("\n");
   }
  
+  for (int i = 0; i < 10; i++){
+    printf("V[%i]: ", i);
+    printf("%d\n", v[i]);
+  }
+ 
   long thread;
-  pthread_t *handle;
+  pthread_t *handle;;
     
   count_t = strtol(argv[1], NULL, 10);
   handle = malloc(count_t * sizeof(pthread_t));
@@ -64,4 +64,5 @@ int main(int argc, char *argv[]) {
 
   free(handle);
 }
+
 
