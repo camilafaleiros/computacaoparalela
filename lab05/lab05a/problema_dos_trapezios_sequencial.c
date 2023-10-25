4abad3aeb3a68563e4d94359df399fbbd4446032
Cloud User <ec2-user@ip-172-31-89-49.ec2.internal>
2023-10-25 19:04:49 +0000

    correção de erros

--- lab05/lab05a/problema_dos_trapezios_sequencial.c
+++ lab05/lab05a/problema_dos_trapezios_sequencial.c
@@ -9,12 +9,14 @@ double f(double x) {
 
 double Trap(int n, double a, double b) {
     double h = (b - a) / n;
-    double result = (f(a) + f(b)) / 2.0;
+    double result = 0.0;
 
+    result += f(a) / 2.0; // Adiciona a primeira parte
     for (int i = 1; i < n; i++) {
         double x = a + i * h;
         result += f(x);
     }
+    result += f(b) / 2.0; // Adiciona a última parte
 
     result *= h;
 
@@ -36,11 +38,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    double result = 0.0;
+
     clock_t start_time = clock();
 
-    double result = Trap(n, a, b);
+    #pragma omp parallel num_threads(1)
+    {
+        double local_result = Trap(n, a, b);
+
+        #pragma omp critical
+        result += local_result;
+    }
 
     clock_t end_time = clock();
+
     double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
     printf("With n = %d trapezoids, our estimate\n", n);
@@ -49,5 +60,4 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
 
